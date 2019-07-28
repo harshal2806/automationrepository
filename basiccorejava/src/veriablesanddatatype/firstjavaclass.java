5489e9c76852e3ff7c0e0477e639cf0cbd3a9cfd
rakhi <rakhianaya@2015>
2019-07-28 10:07:31 +0530

    committing my first java class

---
+++ basiccorejava/src/veriablesanddatatype/firstjavaclass.java
@@ -0,0 +1,10 @@
+package veriablesanddatatype;
+
+public class firstjavaclass {
+
+	public static void main(String[] args) {
+		
+       System.out.println("Hello Selenium");
+	}
+
+}
---
+++ basiccorejava/src/veriablesanddatatype/secondjavaclass.java
@@ -0,0 +1,10 @@
+package veriablesanddatatype;
+
+public class secondjavaclass {
+
+	public static void main(String[] args) {
+		System.out.println("second selenium class");
+
+	}
+
+}
