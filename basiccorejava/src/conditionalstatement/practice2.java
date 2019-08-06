07cf931b2ebae024687f2e12bea74f19d1ea31dd
Unknown <unknown@example.com>
2019-08-06 22:14:05 +0530

    commit my practice 2

---
+++ basiccorejava/src/conditionalstatement/practice2.java
@@ -0,0 +1,29 @@
+package conditionalstatement;
+
+public class practice2 
+{
+
+	public static void main(String[] args)
+	{
+	int a=20,b=20;
+//	if(a==b)
+//	{System.out.println("a is equal to b");
+//	}else
+//	{System.out.println("a is not equal to b");
+//	
+//	}
+//	if(a!=b)
+//	{System.out.println("a is not equal to");
+//	}else
+//	{System.out.println("a is equal to b");
+//	}
+	if(a<=b)
+	{System.out.println("a is less than of equal to b");
+	}else {
+		System.out.println("a is greater than b");
+		
+	}
+	
+	}
+
+}
