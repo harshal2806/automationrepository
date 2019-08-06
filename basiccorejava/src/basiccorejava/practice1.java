d76fc310cc1ff9ec1f41397afc29546df5aae5fd
Unknown <unknown@example.com>
2019-08-06 22:11:31 +0530

    commit pracice1 file

---
+++ basiccorejava/src/basiccorejava/practice1.java
@@ -0,0 +1,12 @@
+package basiccorejava;
+
+public class practice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10, b=20,c=30,d=40;
+		System.out.println(a*b-c*d);
+
+	}
+
+}
